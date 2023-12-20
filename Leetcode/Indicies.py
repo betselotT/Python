4b9eefec9d3696df627102012c8d8611083c575c
Betselot Tesfa <betselottesfa9@gmail.com>
2023-12-19 19:12:58 -0900

    indicies

---
+++ Leetcode/Indicies.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        arr = []
+        for i, word in enumerate(words):
+            if x in word:
+                arr.append(i)
+        return arr
