26fae440aa5c225ac4ca947d9fd853316fbb8dac
Betselot Tesfa <betselottesfa9@gmail.com>
2023-12-26 10:34:42 -0900

    pivot

--- Codeforces/Temp.py
+++ Codeforces/Temp.py
@@ -1,1 +1,6 @@
-num =
+if 'P' not in garbage[-1]: 
+            TravelP -= travel[-1]
+        if 'G' not in garbage[-1]:
+            TravelG -= travel[-1]
+        if 'M' not in garbage[-1]:
+            TravelM -= travel[-1]
