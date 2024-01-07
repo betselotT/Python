b233d7d62bf91c89bc72c405fc3bbb88334efe6b
Betselot Tesfa <betselottesfa9@gmail.com>
2024-01-07 06:49:40 -0900

    69

---
+++ Leetcode/Easy/69.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        arr = []
+        number = list(str(num))
+        for i in range(len(number)):
+            if number[i] == '6':
+                number[i] = '9'
+                break
+        return int(''.join(number))
