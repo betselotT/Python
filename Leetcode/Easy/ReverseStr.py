9bf1655f7e52728947959864ef0f5597bdce914b
betselotT <betselottesfa9@gmail.com>
2024-02-19 13:44:25 -0300

    winner

---
+++ Leetcode/Easy/ReverseStr.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseString(self, arr: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        start = 0
+        end = len(arr) - 1
+        while start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+            start += 1
+            end -= 1
+        print(arr)
